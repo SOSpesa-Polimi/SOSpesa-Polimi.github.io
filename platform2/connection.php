c71cf729683282de97271652b9ff003be7ad9a85
SOSpesa-Polimi <116626956+SOSpesa-Polimi@users.noreply.github.com>
2022-10-25 09:36:14 +0200

    Delete platform2 directory

--- platform2/connection.php
+++
@@ -1,3 +0,0 @@
-<?php
-$manager = new MongoDB\Driver\Manager("mongodb+srv://Siveq:Polisocial@cluster0-bakfq.gcp.mongodb.net/test?retryWrites=true");
-?>
--- platform2/isLogin.php
+++
@@ -1,7 +0,0 @@
-<?php
-           session_start();
-           $user=$_SESSION['user'];
-           if(!isset($_SESSION['user'])){
-             header("Location: register.php");
-           }
-?>
